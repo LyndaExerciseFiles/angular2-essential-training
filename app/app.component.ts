98ab16d9890cab979a1aade698899a2897f2c58e
wesniem <wesniem@yahoo.com>
2016-12-31 15:17:13 -0500

    Using TemplateUrl

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app',
-  template: '<h1>My App</h1>'
+  selector: 'mw-app',
+  templateUrl:'app/app.component.html'
 })
 export class AppComponent {}
