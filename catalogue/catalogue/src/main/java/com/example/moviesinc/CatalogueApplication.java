345320d2aaa3a1bf39bcc1fd6cbfa03e498f2581
himawan-r <IMadeHimawan.Raharja@mitrais.com>
2018-06-08 15:05:46 +0800

    update pom, remove unused import

--- catalogue/catalogue/src/main/java/com/example/moviesinc/CatalogueApplication.java
+++ catalogue/catalogue/src/main/java/com/example/moviesinc/CatalogueApplication.java
@@ -1,9 +1,5 @@
 package com.example.moviesinc;
 
-import com.example.moviesinc.catalogue.Model.Image;
-import com.example.moviesinc.catalogue.Model.MediaItem;
-import com.example.moviesinc.catalogue.Service.MediaItemService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +8,6 @@ public class CatalogueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogueApplication.class, args);
-
 	}
 
 }
