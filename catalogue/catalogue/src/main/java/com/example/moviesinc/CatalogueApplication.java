e1026c339de4f96a29d4b320bcd56bd40f43ff5c
himawan-r <IMadeHimawan.Raharja@mitrais.com>
2018-06-11 14:13:04 +0800

    Revert changes on servlet initializer

--- catalogue/catalogue/src/main/java/com/example/moviesinc/CatalogueApplication.java
+++ catalogue/catalogue/src/main/java/com/example/moviesinc/CatalogueApplication.java
@@ -2,16 +2,9 @@ package com.example.moviesinc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CatalogueApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
-		return application.sources(CatalogueApplication.class);
-	}
+public class CatalogueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogueApplication.class, args);
