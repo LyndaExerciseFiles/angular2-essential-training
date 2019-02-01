1793b69a2320b7b07e74c318a56d8266e2d52906
himawan-r <IMadeHimawan.Raharja@mitrais.com>
2019-02-01 09:38:19 +0800

    move ui folder to resources

--- catalogue/catalogue/src/main/java/com/example/moviesinc/catalogue/WS/MediaItemWs.java
+++ catalogue/catalogue/src/main/java/com/example/moviesinc/catalogue/WS/MediaItemWs.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Service("mediaItemWs")
 @RestController
